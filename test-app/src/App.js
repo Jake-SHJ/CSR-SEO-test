3fc88b2fe959c2ab4a371069beafc101e7e5f636
jake <guswns1853@gmail.com>
2020-10-11 23:04:02 +0900

    react-helmet test

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Helmet>
+        <title>리액트 헬멧을 이용한 타이틀 변경</title>
+      </Helmet>
+      Test 헬멧
+    </div>
+  );
 }
 
 export default App;
